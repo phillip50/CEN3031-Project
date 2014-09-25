3c5bec50e6408c87613db1eba30c5de87002fd96
Megan <megddwyer@gmail.com>
2014-09-25 15:40:26 -0400

    Got the dummy angular data working!
    Hooked up the controller correctly to dynamically create a table of
    data (to become bug pics late)

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -12,5 +12,13 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			},
 			zoom: 8
 		};
+				$scope.phones = [
+		    {'name': 'Nexus S',
+		     'snippet': 'Fast just got faster with Nexus S.'},
+		    {'name': 'Motorola XOOM™ with Wi-Fi',
+		     'snippet': 'The Next, Next Generation tablet.'},
+		    {'name': 'MOTOROLA XOOM™',
+		     'snippet': 'The Next, Next Generation tablet.'}
+	  	];
 	}
 ]);
