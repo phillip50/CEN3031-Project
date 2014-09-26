eb8483c44c90f48ee5e997aa77a362db6900168b
Abraham <at.thehotcorner@gmail.com>
2014-09-26 15:40:55 -0400

    Added InfoWindows to markers on map

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -12,7 +12,10 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			},
 			zoom: 14,
 			bounds: {},
-			options: {scrollwheel: true}
+			options: {
+				scrollwheel: true,
+				streetViewControl: false
+			}
 		};
 		/*$scope.marker = {
 			id: 0,
@@ -39,30 +42,23 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		};
 
 		var createRandomMarker = function (i, bounds, bug, idKey) {
-            //console.log(bug);
-			var lat_min = bounds.southwest.latitude,
-                lat_range = bounds.northeast.latitude - lat_min,
-                lng_min = bounds.southwest.longitude,
-                lng_range = bounds.northeast.longitude - lng_min;
-
-            if (idKey == null) {
-                idKey = "id";
-            }
-
-            var latitude = lat_min + (Math.random() * lat_range);
-            var longitude = lng_min + (Math.random() * lng_range);
-            // Note, the label* properties are only used if isLabel='true' in the directive.
+            if (idKey == null) idKey = "id";
 			var ret = {
 				options: {
-					draggable: true,
+					draggable: false,
 					labelAnchor: '10 39',
 					labelContent: i,
 					labelClass: 'labelMarker'
 				},
-				latitude: latitude,
-				longitude: longitude,
-				title: 'm' + i,
-				icon: bug.pic
+				latitude: bug.coords.latitude,
+				longitude: bug.coords.longitude,
+				title: bug.name,
+				caughtBy: bug.caughtBy,
+				location: bug.location
+				//icon: bug.pic
+			};
+			ret.onClick = function() {
+				ret.show = !ret.show;
 			};
             ret[idKey] = i;
             return ret;
@@ -91,8 +87,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			caughtBy : 'Ash',
 			location : 'Pallet Town',
 			coords: {
-				latitude: 29.6398801,
-				longitude: -82.3551082
+				latitude: 29.631146633445802,
+				longitude: -82.34787039550469
 			}
 		}, {
 			id: 1,
@@ -101,8 +97,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			caughtBy : 'Bugsy',
 			location : 'Gainesville',
 			coords: {
-				latitude: 29.6398801,
-				longitude: -82.3551082
+				latitude: 29.64261231,
+				longitude: -82.348432409
 			}
 		}, {
 			id: 2,
@@ -111,8 +107,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			caughtBy : 'Brock',
 			location : 'Hawaii',
 			coords: {
-				latitude: 29.6398801,
-				longitude: -82.3551082
+				latitude: 29.6328801,
+				longitude: -82.3521082
 			}
 		}, {
 			id: 3,
@@ -121,8 +117,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			caughtBy : 'Misty',
 			location : 'Somewhere',
 			coords: {
-				latitude: 29.6398801,
-				longitude: -82.3551082
+				latitude: 29.6308801,
+				longitude: -82.3451082
 			}
 		}, {
 			id: 4,
@@ -131,8 +127,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			caughtBy : 'Jessie',
 			location : 'Nowhere',
 			coords: {
-				latitude: 29.6398801,
-				longitude: -82.3551082
+				latitude: 29.6368801,
+				longitude: -82.3531082
 			}
 		}, {
 			id: 5,
@@ -141,8 +137,8 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 			caughtBy : 'Jessie',
 			location : 'Nowhere',
 			coords: {
-				latitude: 29.6398801,
-				longitude: -82.3551082
+				latitude: 29.6328801,
+				longitude: -82.3511082
 			}
 		}];
 	}
