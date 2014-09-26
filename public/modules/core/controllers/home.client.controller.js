f8c157c5e3e7d75edf96bf42bead25c59b243164
Megan <megddwyer@gmail.com>
2014-09-26 13:13:41 -0400

    Text appears on hover
    Edited Buttons

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -14,14 +14,24 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		};
 		$scope.sample_bugs = [
 		    {'name': 'Butterfry',
+		    'caughtBy' : 'Ash',
+		    'location' : 'Pallet Town',
 		     'pic': 'bug1.png'},
 		    {'name': 'Ledyba',
+		    'caughtBy' : 'Bugsy',
+		    'location' : 'Gainesville',
 		     'pic': 'bug2.jpg'},
 		    {'name': 'Caterpie',
+		    'caughtBy' : 'Brock',
+		    'location' : 'Hawaii',
 		     'pic': 'bug3.png'},
 		    {'name': 'Weedle',
+		    'caughtBy' : 'Misty',
+		    'location' : 'Somewhere',
 		     'pic': 'bug4.jpg'},
 		    {'name': 'Galvantula',
+		    'caughtBy' : 'Jessie',
+		    'location' : 'Nowhere',
 		     'pic': 'bug5.png'}
 	  	];
 	}
