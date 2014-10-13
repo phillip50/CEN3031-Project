e0de7f0c2616465066d9d421f17ea6de37ffd3e2
Megan <megddwyer@gmail.com>
2014-10-13 12:31:40 -0400

    hide broken articles tab

--- public/modules/articles/config/articles.client.config.js
+++ public/modules/articles/config/articles.client.config.js
@@ -4,8 +4,10 @@
 angular.module('articles').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items (Hidden for presentation)
+		/*
 		Menus.addMenuItem('topbar', '', 'articles', 'dropdown', '/articles(/create)?');
 		Menus.addSubMenuItem('topbar', 'articles', 'List Articles', 'articles');
 		Menus.addSubMenuItem('topbar', 'articles', 'New Article', 'articles/create');
+		*/
 	}
 ]);
---
+++ public/modules/insects/controllers/gallerys.client.controller.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('gallerys').controller('GallerysController', ['$scope', '$stateParams', '$location', 'Authentication', 'Gallerys',
+	function($scope, $stateParams, $location, Authentication, Gallerys) {
+		$scope.authentication = Authentication;
+
+		$scope.create = function() {
+			var gallery = new Gallerys({
+				title: this.title,
+				content: this.content,
+				//insects.push(this.insect)
+			});
+			gallery.$save(function(response) {
+				$location.path('gallerys/' + response._id);
+
+				$scope.title = '';
+				$scope.content = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		$scope.remove = function(gallery) {
+			if (gallery) {
+				gallery.$remove();
+
+				for (var i in $scope.gallerys) {
+					if ($scope.gallerys[i] === gallery) {
+						$scope.gallerys.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.gallery.$remove(function() {
+					$location.path('gallerys');
+				});
+			}
+		};
+
+		$scope.update = function() {
+			var gallery = $scope.gallery;
+
+			gallery.$update(function() {
+				$location.path('gallerys/' + gallery._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		$scope.find = function() {
+			$scope.gallerys = Gallerys.query();
+		};
+
+		$scope.findOne = function() {
+			$scope.gallery = Gallerys.get({
+				galleryId: $stateParams.galleryId
+			});
+		};
+	}
+]);
