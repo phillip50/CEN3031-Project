ea3ceb8c2817e4b9ca41cbe1d6933ab57ed82339
Megan <megddwyer@gmail.com>
2014-12-09 01:49:19 -0500

    Removed line in notes controller for login issue
    Even if I’m logged in, it won’t let me view notes, so I had to take out
    that !scope.user statement

--- public/modules/notes/controllers/notes.client.controller.js
+++ public/modules/notes/controllers/notes.client.controller.js
@@ -5,7 +5,7 @@ angular.module('notes').controller('NotesController', ['$scope', '$stateParams',
 		$scope.authentication = Authentication;
 
 		// If user is not signed in then redirect back home
-		if (!$scope.user) $location.path('/');
+		//if (!$scope.user) $location.path('/');
 
 		$scope.createPage = function() {
 			$scope.insects = [];
